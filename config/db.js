f19d7f34759bded6a9b9c8c5a8d8924d6e56a72f
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2025-02-05 20:39:53 +0200

    first

--- config/db.js
+++ config/db.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('MongoDB connection error:', err);
+        console.log('MONGODB_URI:', process.env.MONGODB_URI);
         process.exit(1);
     }
 };
